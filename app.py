ce978ac3fbbef96d0d2af4e6dd4b449f731d5717
Paul Brenot-Vinciguerra <paul.brenot-vinciguerra@iqo.eu>
2023-08-11 19:16:12 +0200

    cleaning background image

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if question:
         key="container_with_border",
         css_styles="""
             {
-                border: 1px solid rgba(49, 51, 63, 0.2);
+                border: 1px solid rgba(239, 239, 240, 1);
                 border-radius: 0.5rem;
                 padding: calc(1em - 1px)
             }
