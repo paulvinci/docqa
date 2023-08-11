60fe0eed672ae0c4f82bed4c735cb2c7e838fbb4
Paul Brenot-Vinciguerra <paul.brenot-vinciguerra@iqo.eu>
2023-08-11 19:10:21 +0200

    cleaning background image

--- app.py
+++ app.py
@@ -47,6 +47,6 @@ if question:
                 padding: calc(1em - 1px)
             }
             """):
-        stream_example(response.text)
+        st.markdown(response.text)
     st.info(f'Execution time: {exec_time} minutes')
     st.slider('Evaluate answer - 5 being excellent answer',1,5)
