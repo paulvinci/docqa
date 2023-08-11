3fb099b6ace2b5d56d734dbf3410f02e3935c9b5
Paul Brenot-Vinciguerra <paul.brenot-vinciguerra@iqo.eu>
2023-08-11 18:41:24 +0200

    updating st format

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Bring in deps
-import streamlit as st 
+import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
 import time
 import os
 import requests
@@ -13,23 +14,41 @@ from langchain import PromptTemplate
 
 
 # Customize the layout
-st.set_page_config(page_title="RuLLM", page_icon="👑", layout="wide")     
+st.set_page_config(page_title="RuLLM", page_icon="👑", layout="wide") 
+image_url = "https://images.unsplash.com/photo-1682685797828-d3b2561deef4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
 st.markdown(f"""
             <style>
-            .stApp {{background-image: url("https://images.unsplash.com/photo-1682685797366-715d29e33f9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"); 
+            .stApp {{background-image: url(f"{image_url}"); 
                      background-attachment: fixed;
                      background-size: cover}}
          </style>
          """, unsafe_allow_html=True)
 
+def stream_example(response_text):
+    for word in response_text.split():
+        yield word + " "
+        time.sleep(0.1)
+
 # Query through LLM    
+ngrok_url = "https://5ad1-81-67-151-153.ngrok-free.app/"
 question = st.text_input("Ask something from the file")    
 if question:
     tic = time.perf_counter()
-    url = f'https://5ad1-81-67-151-153.ngrok-free.app/search?query={question}'
+    url = f'{ngrok_url}search?query={question}'
     response = requests.get(url)
     toc = time.perf_counter()
     exec_time = time.strftime("%M:%S", time.gmtime(toc - tic))
-    st.info(response.text)
+    #st.info(response.text)
+    with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+            }
+            """,
+    ):
+        st.markdown(stream_example(response.text)
     st.info(f'Execution time: {exec_time} minutes')
     st.slider('Evaluate answer - 5 being excellent answer',1,5)
