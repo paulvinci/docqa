d03786f22c8792fd4f5225ead911842cc4e86b3c
Paul Brenot-Vinciguerra <paul.brenot-vinciguerra@iqo.eu>
2023-08-12 14:14:53 +0200

    adding gsheet

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if question:
  
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
+    return pd.read_csv(csv_url, on_bad_lines='skip')
 df = load_data(st.secrets["public_gsheets_url"])
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
