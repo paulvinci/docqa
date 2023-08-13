51fd33d7ab11580346cb346e7a165059d7cb7d43
Paul Brenot-Vinciguerra <paul.brenot-vinciguerra@iqo.eu>
2023-08-13 05:50:25 +0200

    adding new gsheet

--- app.py
+++ app.py
@@ -57,11 +57,11 @@ if question:
 ## Loading
 spreadsheet_key = "1lvIK4MoqqRLeIFe4XstOF8GKxpuuB9mFeauMho6jgjw"
 scope = "https://spreadsheets.google.com/feeds"
-step_1 = str(st.secrets["gcp_service_account"])
-st.write(step_1)
-step_2 = open(step_1)
-step_3 = toml.loads(step_2)
-#credentials_json = json.loads(json.dumps(toml.loads(open(str(st.secrets["gcp_service_account"])).read())))
+#step_1 = str(st.secrets["gcp_service_account"])
+#st.write(step_1)
+#step_2 = open(step_1)
+#step_3 = toml.loads(step_2)
+credentials_json = json.loads(json.dumps(toml.loads(str(st.secrets["gcp_service_account"])).read())))
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json, scope)
 worksheet = gspread.authorize(credentials).open_by_key(spreadsheet_key).worksheet("Feuille 1")
 data = worksheet.get_all_values()
